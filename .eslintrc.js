6426c4e8861edad13dde3964879c29787a781e0e
Dmytro Fedin <dimitriyfedin@gmail.com>
2023-02-19 13:16:33 +0200

    test

--- .eslintrc.js
+++ .eslintrc.js
@@ -33,6 +33,7 @@ module.exports = {
     'react/jsx-props-no-spreading': 'warn',
     'react/function-component-definition': 'off',
     'no-shadow': 'off',
+    'linebreak-style': 0,
     'import/extensions': 'off',
     'import/no-extraneous-dependencies': 'off',
     'no-underscore-dangle': 'off',
